f3b114d7b8ff8aaca14cc6a1c579482544474da7
apisitgosaswiang-sudo <apisit.gosaswiang@gmail.com>
2026-07-22 09:33:31 +0700

    Delete program-builder.js

--- program-builder.js
+++
@@ -1,5 +0,0 @@
-# Hotfix QA
-
-Test Member: Today, Workout, Progress, Photos, Profile.
-
-Test Trainer: Dashboard avatar, Members Add/Edit, Monthly Package, Photos read-only, Programs, Library, Settings, Beta Control.
